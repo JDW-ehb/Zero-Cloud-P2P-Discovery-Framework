0a42e8a6e81686851f04e14f3eb5413beb5b2b7e
Luca Raymaekers <luca@spacehb.net>
2026-02-15 21:01:21 +0100

    Added settingspage

---
+++ ZCM/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics;
+using ZCL.API;
+namespace ZCM.Pages;
+
+public partial class SettingsPage : ContentPage
+{
+	public SettingsPage()
+	{
+		InitializeComponent();
+
+        BindingContext = Config.Instance;
+    }
+
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        Debug.WriteLine("Saved.");
+    }  
+}
